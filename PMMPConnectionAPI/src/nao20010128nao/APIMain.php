22ead6dd15b6bc1fec70bfe0132e134cea4da3ea
nao20010128nao <nao20010128@gmail.com>
2015-05-28 23:35:53 +0900

    通信APIの作成開始

---
+++ PMMPConnectionAPI/src/nao20010128nao/APIMain.php
@@ -0,0 +1,34 @@
+<?php
+namespace nao20010128nao;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Listener;
+
+class APIMain extends PluginBase implements Listener{
+	private $config;
+	private $cSender;
+	public function onEnable(){
+		$this->cSender=new ConsoleCommandSender();
+		if(!file_exists($this->getDataFolder())){
+			mkdir($this->getDataFolder());
+		}
+		if(file_exists($this->getDataFolder()."config.yml")){
+			$this->config=yaml_parse_file($this->getDataFolder()."config.yml");
+			if($this->config===false){
+				$this->config=array("server"->array(
+						"ip"->"nao20010128nao.dip.jp",
+						"port"->20200
+					));
+			}
+		}else{
+			$this->config=array("server"->array(
+						"ip"->"nao20010128nao.dip.jp",
+						"port"->20200
+					));
+		}
+	}
+	public function onDisable(){
+		
+	}
+}
