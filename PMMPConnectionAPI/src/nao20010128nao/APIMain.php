c1e98a296ff14d7f4d1bc99e7f36e3520c6794db
nao20010128nao <nao20010128@gmail.com>
2015-05-29 23:44:19 +0900

    通信APIプラグイン完成

--- PMMPConnectionAPI/src/nao20010128nao/APIMain.php
+++ PMMPConnectionAPI/src/nao20010128nao/APIMain.php
@@ -5,12 +5,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class APIMain extends PluginBase implements Listener{
 	private $config;
 	private $cSender;
 	private $connection;
 	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->cSender=new ConsoleCommandSender();
 		$this->cSender->sendMessage(TextFormat::GREEN."Loading config...");
 		if(!file_exists($this->getDataFolder())){
@@ -33,9 +35,21 @@ class APIMain extends PluginBase implements Listener{
 		$this->connection=new Connection($this->config,$this);
 		$this->cSender->sendMessage(TextFormat::GREEN."Pinging...");
 		$data=$this->connection->postData("ping",array());
-		
+		if($data===false){
+			$this->cSender->sendMessage(TextFormat::RED."Failed to connect! Is the server running?");
+		}else if($data==Consistants::PING_RESULT){
+			$this->cSender->sendMessage(TextFormat::GREEN."Ping OK!");
+		}else{
+			$this->cSender->sendMessage(TextFormat::RED."Response Error! Is the server correct?");
+		}
 	}
 	public function onDisable(){
-		
+		yaml_emit_file($this->getDataFolder()."config.yml",$this->config);
+	}
+	public function onPlayerJoin(PlayerJoinEvent $event){
+		$player=$event->getPlayer();
+		$name=$player->getName();
+		$address=$player->getAddress();
+		$cid=$player->getUniqueId();
 	}
 }
